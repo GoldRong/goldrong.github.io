38a38371713e5be9e2a15533ca7056e4c65ff243
xurong <547217475@qq.com>
2018-05-13 11:27:42 +0800

    完成vuepress模版搭建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: '聚散流沙',
+    title: '知识树',
     description: '把代码写复杂很容易，把代码写简单很难，这里讲的是如何编写简单易读的代码',
     dest: './dist',    // 设置输出目录
     base: '/', // 设置站点根路径
@@ -7,33 +7,26 @@ module.exports = {
         repo: 'https://github.com/xurong/xurong.github.io' ,
         nav: [
             { text: '首页', link: '/' },
-            { text: '编程规范', link: '/foo/' },
+            { text: '算法', link: '/foo/' },
             { text: '数据结构', link: '/bar/' }
         ],
         sidebar: {
             '/foo/': [{
-                title: '程序员你为什么这么累',
-                collapsable: true,
+                title: '算法',
+                collapsable: false,
                 children: [
                     '',
-                    'rest-defined',
-                    'controller',
-                    'aop',
-                    'log',
-                    'exception',
-                    'i18n-valid',
-                    'util',
-                    'function',
-                    'config',
-                    'change',
-                    'github'
+                    'one',
+                    'two'
                 ]
             }],
-            '/ajax/': [{
-                title: 'Ajax跨越安全详解',
-                collapsable: true,
+            '/bar/': [{
+                title: '数据结构',
+                collapsable: false,
                 children: [
                     '',
+                    'three',
+                    'four'
                 ]
             }]
         }
