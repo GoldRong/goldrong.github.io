c48138e005f15fbca2590dbcff8853145c7e01e7
xurong <xurong@sunmi.com>
2018-09-04 14:32:34 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,15 +8,7 @@ module.exports = {
         nav: [
             {text: '首页', link: '/'},
             {text: 'POS金融工具', link: 'http://114.67.236.168/#/BitmapViewer'},
-            {
-                text: '数据结构和算法',
-                items: [
-                    {text: '算法', link: 'http://www.cnblogs.com/chengxiao/category/880910.html'},
-                    {text: '张晓康', link: 'https://zhuanlan.zhihu.com/p/37289934'},
-                    {text: '赵海英',link: 'https://www.bilibili.com/video/av18777526/?from=search&seid=12584358532423887040'},
-                    {text: '机器学习算法', link: 'https://feisky.xyz/machine-learning/'}
-                ]
-            },
+
             {
                 text: 'Android',
                 items: [
@@ -26,15 +18,32 @@ module.exports = {
                 ]
             },
             {
+                text: '数据结构和算法',
+                items: [
+
+                    {text: 'JCSprout', link: 'https://github.com/crossoverJie/JCSprout'},
+                    {text: '后端架构师图谱', link: 'https://github.com/xingshaocheng/architect-awesome'},
+                    {text: '二叉搜索树', link: 'http://www.ganecheng.tech/'},
+                    {text: '算法', link: 'http://www.cnblogs.com/chengxiao/category/880910.html'},
+                    {text: '张晓康', link: 'https://zhuanlan.zhihu.com/p/37289934'},
+                    {
+                        text: '赵海英',
+                        link: 'https://www.bilibili.com/video/av18777526/?from=search&seid=12584358532423887040'
+                    },
+                    {text: '机器学习算法', link: 'https://feisky.xyz/machine-learning/'}
+                ]
+            },
+            {
                 text: 'JAVA',
                 items: [
                     {text: '尚硅谷_SpringCloud（全）', link: 'https://www.bilibili.com/video/av22613028'},
                     {text: '尚硅谷SpringBoot全集', link: 'https://www.bilibili.com/video/av23478787'},
-                    {text: '史上最全设计模式导学', link: 'https://blog.csdn.net/lovelion/article/details/17517213?hmsr=toutiao.io&utm_medium=toutiao.io&utm_source=toutiao.io'},
                     {text: 'Java3y', link: 'https://www.jianshu.com/u/c4503bc2c490'},
                     {text: '纯洁的微笑', link: 'http://www.ityouknow.com/archives.html'},
-                    {text: '后端架构师图谱', link: 'https://github.com/xingshaocheng/architect-awesome'},
-                    {text: 'ganecheng', link: 'http://www.ganecheng.tech/'}
+                    {
+                        text: '史上最全设计模式导学',
+                        link: 'https://blog.csdn.net/lovelion/article/details/17517213?hmsr=toutiao.io&utm_medium=toutiao.io&utm_source=toutiao.io'
+                    },
                 ]
             },
             {
