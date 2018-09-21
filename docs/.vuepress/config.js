1477bae694704c5f4089197d6b5f9fa33dbc3b64
xurong <xurong@sunmi.com>
2018-09-21 15:36:59 +0800

    更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,14 +6,10 @@ module.exports = {
     themeConfig: {
         repo: 'https://github.com/xurong/xurong.github.io',
         nav: [
-            {
-                text: '先知',
-                items: [
-                    {text: 'POS金融工具', link: 'http://114.67.236.168/#/BitmapViewer'},
-                    {text: 'teambition', link: 'https://www.teambition.com/organization/55263514a7d66b384f02e9a1'},
-                    {text: 'rap2', link: 'http://rap2.taobao.org/repository'}
-                ]
-            },
+
+            {text: 'POS金融工具', link: 'http://114.67.236.168/#/BitmapViewer'},
+            {text: 'teambition', link: 'https://www.teambition.com/organization/55263514a7d66b384f02e9a1'},
+            {text: 'rap2', link: 'http://rap2.taobao.org/repository'},
 
             {
                 text: 'Android',
