1d9d2a7bd5c37d2046133cb10c6d3db74684791d
xurong <xurong@sunmi.com>
2018-11-08 14:58:02 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,15 +23,13 @@ module.exports = {
                 text: '数据结构和算法',
                 items: [
                     {text: 'JCSprout', link: 'https://github.com/crossoverJie/JCSprout'},
+                    {text: 'JavaGuide', link: 'https://github.com/Snailclimb/JavaGuide'},
                     {text: '后端架构师图谱', link: 'https://github.com/xingshaocheng/architect-awesome'},
-                    {text: '二叉搜索树', link: 'http://www.ganecheng.tech/'},
                     {text: '算法', link: 'http://www.cnblogs.com/chengxiao/category/880910.html'},
-                    {text: '张晓康', link: 'https://zhuanlan.zhihu.com/p/37289934'},
                     {
-                        text: '赵海英',
-                        link: 'https://www.bilibili.com/video/av18777526/?from=search&seid=12584358532423887040'
-                    },
-                    {text: '机器学习算法', link: 'https://feisky.xyz/machine-learning/'}
+                        text: '史上最全设计模式导学',
+                        link: 'https://blog.csdn.net/lovelion/article/details/17517213?hmsr=toutiao.io&utm_medium=toutiao.io&utm_source=toutiao.io'
+                    }
                 ]
             },
             {
@@ -40,11 +38,7 @@ module.exports = {
                     {text: '尚硅谷_SpringCloud（全）', link: 'https://www.bilibili.com/video/av22613028'},
                     {text: '尚硅谷SpringBoot全集', link: 'https://www.bilibili.com/video/av23478787'},
                     {text: 'Java3y', link: 'https://www.jianshu.com/u/c4503bc2c490'},
-                    {text: '纯洁的微笑', link: 'http://www.ityouknow.com/archives.html'},
-                    {
-                        text: '史上最全设计模式导学',
-                        link: 'https://blog.csdn.net/lovelion/article/details/17517213?hmsr=toutiao.io&utm_medium=toutiao.io&utm_source=toutiao.io'
-                    },
+                    {text: '纯洁的微笑', link: 'http://www.ityouknow.com/archives.html'}
                 ]
             },
             {
